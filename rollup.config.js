781d0821988b08a91d01665ae9f8ef36205326ca
Firas Rafislam <firasrafislam@live.com>
2022-06-16 14:12:22 +0700

    add production rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,10 @@ const {nodeResolve} = require('@rollup/plugin-node-resolve');
 
 const commonjs = require('@rollup/plugin-commonjs');
 
+const {terser} = require('rollup-plugin-terser');
+
+const postcss = require('rollup-plugin-postcss');
+
 const pagesDir = './src/lib/pages';
 const pages = fs.readdirSync(pagesDir, 'utf-8').map((p) => join(pagesDir, p));
 
@@ -23,9 +27,35 @@ const devConfig = {
     watch: {
         clearScreen: false,
     },
-    plugins: plugins,
+    plugins: [...plugins, postcss()],
 };
 
-export default () => {
-    return devConfig;
-}
+const productionConfig = {
+    input: ['./src/lib/app.js', ...pages],
+    output: {
+      dir: 'dist/js',
+      format: 'esm',
+    },
+    plugins: [
+      ...plugins,
+      postcss({
+        minimize: true,
+      }),
+      terser({
+        format: {
+          // Remove all comments, including @license comments,
+          // otherwise LHCI complains at us.
+          comments: false,
+        },
+      }),
+    ],
+  };
+
+  export default () => {
+    switch (process.env.ELEVENTY_ENV) {
+      case 'prod':
+        return productionConfig;
+      default:
+        return devConfig;
+    }
+  };
