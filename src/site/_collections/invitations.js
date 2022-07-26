445e69fb1287e4f1c927e05ef8682556610bd312
Firas Rafislam <firasrafislam@live.com>
2022-07-26 21:39:34 +0700

    ordering more spaces

--- src/site/_collections/invitations.js
+++ src/site/_collections/invitations.js
@@ -11,7 +11,7 @@ module.exports = () => {
 
     for (const key of keys) {
         const inviteData = invitationData[key];
-        const href = path.join('/', 'rima-ilman', key, '/');
+        const href = path.join('/', 'rimailmanwedding', key, '/');
         const title = `The Wedding of Rima and Ilman`;
         const description = `Kepada Yth: ${inviteData.namaUndangan}`;
 
