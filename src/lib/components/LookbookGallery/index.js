3f5e3891eac1b8fc9e299f25abbacd30806f5387
literasibadguy <firasrafislam@live.com>
2022-09-29 20:24:33 +0700

    add arrow

--- src/lib/components/LookbookGallery/index.js
+++ src/lib/components/LookbookGallery/index.js
@@ -15,16 +15,23 @@ export class LookbookGallery extends BaseElement {
         this.source = "images/feeds/bape-incover-90s.JPG";
 
         this.listenedEvent = this.listenedEvent.bind(this);
+        this.buttonLeftClicked = this.buttonLeftClicked.bind(this);
     }
 
     connectedCallback() {
         super.connectedCallback();
         this.galleryItem = document.querySelector('look-item');
 
+        this.leftButton = document.querySelector('.button-left');
+        this.rightButton = document.querySelector('button-right');
+
+        console.log("Is there any left button", this.leftButton);
+
         this.galleryItems = document.querySelectorAll('look-item');
         for (const item of this.galleryItems) {
             console.log(item)
-            item.addEventListener('item-clicked', this.listenedEvent)
+            item.addEventListener('item-clicked', this.listenedEvent);
+            this.leftButton.addEventListener('click', this.buttonLeftClicked);
         }
         
 
@@ -33,6 +40,10 @@ export class LookbookGallery extends BaseElement {
 
     disconnectedCallback() {
         super.disconnectedCallback()
+    }
+
+    buttonLeftClicked(event) {
+
     }
 
     listenedEvent(event) {
