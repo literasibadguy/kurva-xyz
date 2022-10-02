c5294f734e86a041e32cb82d7f214910f2a35859
literasibadguy <firasrafislam@live.com>
2022-10-02 19:54:34 +0700

    add keydown here

--- src/lib/components/LookbookGallery/index.js
+++ src/lib/components/LookbookGallery/index.js
@@ -13,42 +13,84 @@ export class LookbookGallery extends BaseElement {
     constructor() {
         super();
         this.source = "images/feeds/bape-incover-90s.JPG";
+        this.activeLook = 0;
 
         this.listenedEvent = this.listenedEvent.bind(this);
         this.buttonLeftClicked = this.buttonLeftClicked.bind(this);
+        this.buttonRightClicked = this.buttonRightClicked.bind(this);
     }
 
     connectedCallback() {
         super.connectedCallback();
-        this.galleryItem = document.querySelector('look-item');
 
         this.leftButton = document.querySelector('.button-left');
-        this.rightButton = document.querySelector('button-right');
+        this.rightButton = document.querySelector('.button-right');
+        this.indicator = document.querySelector('#indicator');
 
         console.log("Is there any left button", this.leftButton);
 
         this.galleryItems = document.querySelectorAll('look-item');
         for (const item of this.galleryItems) {
             console.log(item)
-            item.addEventListener('item-clicked', this.listenedEvent);
-            this.leftButton.addEventListener('click', this.buttonLeftClicked);
+            // item.addEventListener('item-clicked', this.listenedEvent);
+            item.addEventListener('item-clicked', (event) => {
+                const index = Array.from(this.galleryItems).indexOf(item)
+                this.listenedEvent(event, index);
+            })
         }
+        this.leftButton.addEventListener('click', this.buttonLeftClicked);
+        this.rightButton.addEventListener('click', this.buttonRightClicked);
         
 
         console.log('this.gallery item', this.galleryItem);
     }
 
     disconnectedCallback() {
-        super.disconnectedCallback()
+        super.disconnectedCallback();
+
+        this.leftButton = document.querySelector('button-left');
+        this.rightButton = document.querySelector('button-right');
+
+        this.galleryItems = document.querySelector('look-item');
+        for (const item of this.galleryItems) {
+            item.removeEventListener('item-clicked', this.listenedEvent);
+        }
+        this.leftButton.removeEventListener('click', this.buttonLeftClicked);
+        this.rightButton.removeEventListener('click', this.buttonRightClicked);
     }
 
     buttonLeftClicked(event) {
+        const items = document.querySelectorAll('look-item');
 
+        if (items[this.activeLook - 1]) {
+          this.activeLook = this.activeLook - 1;
+        } else {
+          this.activeLook = items.length - 1;
+        }
+
+        const item = items[this.activeLook];
+        this.source = item.getAttribute('source');
+
+        this.indicator.innerHTML = `${this.activeLook} of ${items.length}`
     }
 
-    listenedEvent(event) {
+    buttonRightClicked(event) {
+        const items = document.querySelectorAll('look-item');
+        this.activeLook = (this.activeLook + 1) % items.length || 0;
+        const item = items[this.activeLook];
+
+        this.source = item.getAttribute('source');
+        this.indicator.innerHTML = `${this.activeLook} of ${items.length}`
+    }
+
+    listenedEvent(event, index) {
         console.log('event detail', event.detail.source)
-        this.source = event.detail.source
+        this.activeTab = index;
+        this.source = event.detail.source;
+
+        this.indicator.innerHTML = `${index} of 6`
+
+        console.log(this.indicator);
     }
 
     render() {
--- src/lib/components/LookbookGallery/look-item.js
+++ src/lib/components/LookbookGallery/look-item.js
@@ -15,22 +15,58 @@ export class LookItem extends BaseElement {
         super();
 
         this.label = '';
-        this.source = "/feeds/bape-90s-lookbook.jpg";
+        this.source = "";
 
         this.clickedItem = this.clickedItem.bind(this);
     }
 
-    clickedItem() {
+    clickedItem(e) {
+        const item = e.currentTarget;
         console.log("Look clicked")
+
+        item.setAttribute('aria-selected', 'true');
 
         let passImage = new CustomEvent('item-clicked', { bubbles: true, composed: true, detail: { source: this.source }})
 
         this.dispatchEvent(passImage)
     }
 
+    onKeydown(e) {
+        const KEYCODE = {
+            END: 35,
+            HOME: 36,
+            LEFT: 37,
+            RIGHT: 39,
+        }
+
+        switch (e.keyCode) {
+        case KEYCODE.RIGHT:
+            e.preventDefault();
+            console.log("RIGHT KEYCODE");
+            break;
+        case KEYCODE.LEFT:
+            e.preventDefault();
+            console.log("LEFT KEYCODE");
+            break;
+        case KEYCODE.HOME:
+            e.preventDefault();
+            console.log("HOME KEYCODE");
+            break;
+        case KEYCODE.END:
+            e.preventDefault();
+            console.log("END KEYCODE");
+            break;
+        }
+
+    }
+
     render() {
         return html`
-            <img class="gallery-item" src=${this.source} @click=${this.clickedItem} />
+            <img class="gallery-item" decoding="async"
+             loading="lazy"
+             aria-selected="false"
+             @keydown=${this.onkeydown}
+              src=${this.source} @click=${this.clickedItem} />
         `
     }
 }
